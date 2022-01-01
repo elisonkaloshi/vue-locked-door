c5161ecbc6806a206d95b7923a788a33c0e1a347
Elison Kaloshi <elisonkaloshi@gmail.com>
2022-01-01 15:31:25 +0100

    refactor the code

--- src/HasPermission.vue
+++ src/HasPermission.vue
@@ -7,39 +7,36 @@
 <script>
   export default {
     props: {
-        'allPermissions': Array,
-        'permissionsToBeChecked': {
+        allPermissions: Array,
+        permissionsToBeChecked: {
           type: Array,
           required: true
         },
-        'permissionsInStorage': Boolean,
-        'permissionsInStorageKey': String
+        permissionsInStorage: Boolean,
+        permissionsInStorageKey: String
     },
 
     methods: {
       hasPermission() {
         if (this.permissionsInStorage) {
           return this.checkPermissionsFromStorage();
-        } else {
-          return this.checkPermissions();
         }
+        return this.checkPermissions();
       },
 
       checkPermissionsFromStorage() {
         let permissions = JSON.parse(localStorage.getItem(this.permissionsInStorageKey));
         if (Array.isArray(permissions)) {
           return this.permissionsToBeChecked.every(permission => permissions.includes(permission));
-        } else {
-          return false;
         }
+        return false;
       },
 
       checkPermissions() {
         if (Array.isArray(this.allPermissions) && this.allPermissions.length) {
           return this.permissionsToBeChecked.every(permission => this.allPermissions.includes(permission));
-        } else {
-          return false;
         }
+        return false;
       }
     }
   }
